9efbe84f60ba0ca8118ce6bd49f4a013f0760b1b
theo <theodores.indra@ultrasakti.com>
2026-07-06 00:34:38 +0700

    add memoryMessage

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,72 @@
 import {OpenAIClient} from "@anvia/openai"
-import {createCompletion,createParsedCompletion,createCompletionStream} from "@anvia/core"
+import {createCompletion,createParsedCompletion,createCompletionStream, Message} from "@anvia/core"
 import "dotenv/config"
-import { z } from "zod";
+//import { z } from "zod";
+import { input } from "@inquirer/prompts";
 
 const openai = new OpenAIClient({
   apiKey: process.env.MUX_API_KEY,
   baseUrl: process.env.MUX_BASE_URL,
 });
 
-const eventSchema = z.object({
-  title: z.string(),
-  date: z.string(),
-  location: z.string(),
-});
 
-const model = openai.completionModel("gpt-5.5");
+// const eventSchema = z.object({
+//   title: z.string(),
+//   date: z.string(),
+//   location: z.string(),
+// });
 
+ const model = openai.completionModel("gpt-5.5");
 
-const responseCompletion = await createCompletion(model, {
-  instructions: "Always answer in betawi language",
-  input: "Hello, Do you know cibubur?",
+// const responseCompletion = await createCompletion(model, {
+//   instructions: "Always answer in betawi language",
+//   input: "Hello, Do you know cibubur?",
   
-});
+// });
 
-const responseParsedCompletion = await createParsedCompletion(model, {
-  instructions: "Extract the user data, always answer in javanese language",
-  input: "Hello, Do you know Kledokan? on wednesday i want to meet velo and sera at 10am there",
-  schema: eventSchema,
-});
+// const responseParsedCompletion = await createParsedCompletion(model, {
+//   instructions: "Extract the user data, always answer in javanese language",
+//   input: "Hello, Do you know Kledokan? on wednesday i want to meet velo and sera at 10am there",
+//   schema: eventSchema,
+// });
 
 //console.log(responseCompletion.text);
 //console.log(responseParsedCompletion.data);
 
-const responseCompletionStream = await createCompletionStream(model, {
-  instructions: "you are a helpful assistant that can answer questions about the 90s",
-  input: "Hello, Do you know cibubur on 90s?",
-});
+const memoryMessages: Message[] = [];
 
-for await (const chunk of responseCompletionStream) {
-  if (chunk.type === "text_delta") {
-    process.stdout.write(chunk.delta);
+async function main() {
+  while (true) {
+    const userInput = await input({ message: "You: " });
+    if (userInput === "exit") {
+      break;
+    }
+
+    memoryMessages.push(Message.user(userInput));
+
+    const responseCompletionStream =  createCompletionStream(model, {
+      instructions: "you are a helpful assistant, please answer in javanese krama language",
+      input: userInput,
+      messages: memoryMessages,
+    });
+
+    let assistentMessage = "";
+
+    console.log("Assistant: ");
+    for await (const chunk of responseCompletionStream) {
+      if (chunk.type === "text_delta") {
+        process.stdout.write(chunk.delta);
+        assistentMessage += chunk.delta;
+      }
+    }
+    console.log();
+    memoryMessages.push(Message.assistant(assistentMessage));
   }
+
+
+
+
 }
 
-
+main();
 
